fb13d8f1bd185483e5707a06153fd32fd503db1b
Yoder, Tim A <tim.yoder@pnnl.gov>
2024-02-08 15:09:37 -0500

    estimate existing capacity for stock

--- bicep/capacity.py
+++ bicep/capacity.py
@@ -13,11 +13,18 @@ import pandas as pd
 from sqlalchemy import select
 
 from utils.db_models import PeakLoad, query_to_df, engines
+from utils.sampling import utilization_distribution, sample_xstock
+
+PANEL_SIZES = [30, 50, 60, 70, 100, 125, 150, 200, 250, 300, 400, 600, 800, 1000,
+               1200, 2000, 3000, 4000]
 
 
 def building_peak_loads(upgrade=0, residential=1):
-    query = select(PeakLoad).where(PeakLoad.upgrade == upgrade,
-                                   PeakLoad.residential == residential)
+    if residential in (0, 1):
+        query = select(PeakLoad).where(PeakLoad.upgrade == upgrade,
+                                       PeakLoad.residential == residential)
+    else:
+        query = select(PeakLoad).where(PeakLoad.upgrade == upgrade)
     return query_to_df(query)
 
 
@@ -52,7 +59,82 @@ def calc_building_peak_loads():
     building_peak_load_diff(non_zero_upgrade=3, residential=0)
 
 
+class CapacityEstimate:
+    def __init__(self, upgrade, oversize_dist,
+                 residential_voltage=240, commercial_voltage=480, medium_voltage=12470,
+                 max_light_comm_amp=4000,
+                 safety_factor=1.25):
+        self.upgrade = upgrade
+        self.buildings = None
+        self.oversize_distribution = oversize_dist
+        self.safety_factor = safety_factor
+
+        self.resid_volt = residential_voltage
+        self.comm_volt = commercial_voltage
+
+        self.max_comm_amp = max_light_comm_amp
+        self.med_volt = medium_voltage
+
+        self.get_baseline_loads()
+
+    def get_baseline_loads(self):
+        self.buildings = building_peak_loads(self.upgrade, residential=-1)
+
+    def calculate_existing_capacity(self):
+        """Estimate the existing capacity of the baseline stock models"""
+        bldg = self.buildings
+
+        # calculate peak kw from max 15-min interval kwh data
+        bldg['peak_kw'] = bldg['max_elec_consumption_kwh'] / 0.25
+
+        # calculate associate peak amperage and capture assumed voltage
+        bldg['peak_amp'] = 0.0
+        bldg['assume_volt'] = 0
+        bldg.loc[bldg['residential'] == 1, 'peak_amp'] = bldg['peak_kw'] / self.resid_volt * 1000
+        bldg.loc[bldg['residential'] == 1, 'assume_volt'] = self.resid_volt
+
+        bldg.loc[bldg['residential'] == 0, 'peak_amp'] = bldg['peak_kw'] / self.comm_volt * 1000
+        bldg.loc[bldg['residential'] == 0, 'assume_volt'] = self.comm_volt
+
+        # Assume medium voltage (e.g., 12.47 kV) service for large commercial
+        bldg.loc[
+            ((bldg['residential'] == 0) & (bldg['peak_amp'] > self.max_comm_amp)),
+            'assume_volt'] = self.med_volt
+        bldg.loc[bldg['assume_volt'] == self.med_volt, 'peak_amp'] = bldg['peak_kw'] / self.med_volt * 1000
+
+        # calculate required capacity from peak load
+        bldg['req_capacity'] = bldg['peak_amp'] * self.safety_factor
+
+        # generate estimates for current utilization of installed capacity
+        percent_utilized_dist = utilization_distribution()
+        utilization_samples = percent_utilized_dist.resample(size=len(self.buildings))
+        bldg['utilization'] = utilization_samples[0]
+
+        # estimate existing capacity based on utilization draws
+        bldg['est_capacity'] = bldg['peak_amp'] / bldg['utilization']
+
+        # round up to the next largest panel size
+        def round_up_to_panel_size(x):
+            return min(PANEL_SIZES, key=lambda val: (val - x) if val >= x else float('inf'))
+
+        bldg['installed_capacity'] = bldg['est_capacity'].apply(round_up_to_panel_size)
+
+        bldg['spare_capacity'] = bldg['installed_capacity'] - bldg['req_capacity']
+
+
 if __name__ == '__main__':
-    building_peak_load_diff(non_zero_upgrade=4, residential=1)
-    building_peak_load_diff(non_zero_upgrade=6, residential=1)
-    building_peak_load_diff(non_zero_upgrade=3, residential=0)
+    # building_peak_load_diff(non_zero_upgrade=4, residential=1)
+    # building_peak_load_diff(non_zero_upgrade=6, residential=1)
+    # building_peak_load_diff(non_zero_upgrade=3, residential=0)
+
+    import time
+    t0 = time.perf_counter()
+    cap = CapacityEstimate(0, 0)
+    cap.calculate_existing_capacity()
+    t1 = time.perf_counter()
+
+    print(t1 - t0)
+
+    # import plotly.express as px
+    # fig = px.histogram(cap.buildings, x='peak_amp')
+    # fig.show()
