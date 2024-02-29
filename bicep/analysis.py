7941308fae6f5635f0946226524ea41e0210e396
Yoder, Tim A <tim.yoder@pnnl.gov>
2024-02-29 09:27:43 -0500

    results plotting

--- bicep/analysis.py
+++ bicep/analysis.py
@@ -1,6 +1,9 @@
 """
-Aggregation of the total costs at a desired spatial resolution (e.g., state or national).
+Analysis of the BICEP model run results.
 """
+
+import plotly.express as px
+import plotly.graph_objs as go
 
 from bicep.upgrades import UpgradeEstimator
 from utils.sampling import PanelUpgradeCostDistribution
@@ -28,19 +31,73 @@ class BicepResults(UpgradeEstimator):
                          panel_safety_factor=panel_safety_factor)
 
         self.calculate_costs()
+        self._capacity_requirement_cols = ['ev_req_capacity_amp', 'pv_req_capacity_amp',
+                                           'hp_req_capacity_amp', 'hpwh_req_capacity_amp']
 
     def requirements_by_tech(self, residential=1):
-        techs_caps = ['ev_req_capacity_amp', 'pv_req_capacity_amp',
-                      'hp_req_capacity_amp', 'hpwh_req_capacity_amp']
+        dataset = self._filter_dataset(residential)
+        return dataset[self._capacity_requirement_cols].describe()
 
+    def _filter_dataset(self, residential=1):
         if residential == 1:
-            dataset = self.residential
+            return self.residential
         elif residential == 0:
-            dataset = self.commercial
+            return self.commercial
         else:
-            dataset = self.buildings
+            return self.buildings
 
-        return dataset[techs_caps].describe()
+    def plot_drivers(self, residential=1, cdf=True):
+        dataset = self._filter_dataset(residential=residential)
+        plot_df = dataset.rename(columns={
+                              "pv_req_capacity_amp": "PV",
+                              "ev_req_capacity_amp": "EV",
+                              "hp_req_capacity_amp": "HP",
+                              "hpwh_req_capacity_amp": "HP WH"})
+        plot_cols = ['PV', 'EV', 'HP', 'HP WH']
+        if cdf:
+            cdf = px.ecdf(data_frame=plot_df, x=plot_cols)
+            cdf.update_layout(title='Additional capacity requirements by tech',
+                              xaxis_title="Required Capacity [amps]",
+                              yaxis_title="Percentile of Tech",
+                              legend_title="Technologies",)
+            cdf.show()
+        else:
+            histo = go.Figure()
+            pv_cap = dataset['pv_req_capacity_amp']
+            ev_cap = dataset['ev_req_capacity_amp']
+            hp_cap = dataset['hp_req_capacity_amp']
+            hpwh_cap = dataset['hpwh_req_capacity_amp']
+
+            histo.add_trace(go.Histogram(x=pv_cap, name="PV",
+                                         histnorm='percent', nbinsx=100))
+            histo.add_trace(go.Histogram(x=ev_cap, name="EV",
+                                         histnorm='percent', nbinsx=100))
+            histo.add_trace(go.Histogram(x=hp_cap, name="HP",
+                                         histnorm='percent', nbinsx=100))
+            histo.add_trace(go.Histogram(x=hpwh_cap, name="HP WH",
+                                         histnorm='percent', nbinsx=100))
+
+            histo.update_layout(title='Additional capacity requirements by tech',
+                                xaxis_title="Required Capacity [amps]",
+                                yaxis_title="Percentile of Tech",
+                                legend_title="Technologies",
+                                barmode='overlay')
+            histo.update_traces(opacity=0.75)
+            histo.show()
+
+    def plot_peak_amp_distribution(self, residential=1):
+        dataset = self._filter_dataset(residential=residential)
+        histo = px.histogram(dataset, x='peak_amp', title='Estimated Peak Amp Distribution')
+        histo.update_layout(xaxis_title="Peak Capacity [amps]",
+                            yaxis_title="Stock Count")
+        histo.show()
+
+    def plot_spare_capacity(self, residential=1):
+        dataset = self._filter_dataset(residential=residential)
+        histo = px.histogram(dataset, x='spare_capacity', title='Estimated Spare Capacity')
+        histo.update_layout(xaxis_title="Spare Capacity [amps]",
+                            yaxis_title="Stock Count")
+        histo.show()
 
 
 if __name__ == '__main__':
@@ -49,3 +106,10 @@ if __name__ == '__main__':
 
     print(f'total cost for bau: ${bau.total_cost:,.0f}')
     print(f'total cost for high: ${high.total_cost:,.0f}')
+
+    print(f'total residential cost for bau: ${bau.total_residential_costs:,.0f}')
+    print(f'total residential cost for high: ${high.total_residential_costs:,.0f}')
+
+    print(f'total commercial cost for bau: ${bau.total_commercial_costs:,.0f}')
+    print(f'total commercial cost for high: ${high.total_commercial_costs:,.0f}')
+
