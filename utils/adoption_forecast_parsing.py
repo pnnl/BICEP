395a747b58e08b97d7f9f602e9f1c16af285f933
Faye, Bilal <bilal.faye@pnnl.gov>
2025-07-24 16:12:56 -0700

    Map Scout measures to corresponding technology classes in bicep.technologies table

--- utils/adoption_forecast_parsing.py
+++ utils/adoption_forecast_parsing.py
@@ -7,56 +7,56 @@ import json
 
 import pandas as pd
 
-from azure.storage.blob import BlobServiceClient
+# from azure.storage.blob import BlobServiceClient
 
-from utils.sensitive_config import AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY
+# from utils.sensitive_config import AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY
 
-container_name = 'bicep'
-bau_building_blob_name = 'scout-outputs/uec_sdshr_gcam_AEO2023Ref.json'
-high_building_blob_name = 'scout-outputs/uec_sdshr_gcam_alt-High.json'
+# container_name = 'bicep'
+# bau_building_blob_name = 'scout-outputs/uec_sdshr_gcam_AEO2023Ref.json'
+# high_building_blob_name = 'scout-outputs/uec_sdshr_gcam_alt-High.json'
 
-BLOB_URL = account_url = f"https://{AZURE_STORAGE_ACCOUNT}.blob.core.windows.net"
+# BLOB_URL = account_url = f"https://{AZURE_STORAGE_ACCOUNT}.blob.core.windows.net"
 
-service_client = BlobServiceClient(BLOB_URL, credential=AZURE_STORAGE_KEY)
+# service_client = BlobServiceClient(BLOB_URL, credential=AZURE_STORAGE_KEY)
 
 
-def scout_forecast(forecast_blob_name, scenario, metric='stock',):
-    blob_client = service_client.get_blob_client(container=container_name,
-                                                 blob=forecast_blob_name)
+# def scout_forecast(forecast_blob_name, scenario, metric='stock',):
+#     blob_client = service_client.get_blob_client(container=container_name,
+#                                                  blob=forecast_blob_name)
 
-    # Download the blob's content as a stream
-    with BytesIO() as input_blob:
-        blob_client.download_blob().readinto(input_blob)
-        input_blob.seek(0)  # Seek to the start of the stream
-        json_data = json.load(input_blob)
+#     # Download the blob's content as a stream
+#     with BytesIO() as input_blob:
+#         blob_client.download_blob().readinto(input_blob)
+#         input_blob.seek(0)  # Seek to the start of the stream
+#         json_data = json.load(input_blob)
 
-    building_forecast = pd.DataFrame()
-    for state in json_data.keys():
-        if len(json_data[state]) == 0:
-            continue
-        for sector in ['resid', 'comm']:
-            for fuel in ['electricity', 'gas', 'refined liquids', 'biomass']:
-                try:
-                    for end_use in ['heating', 'hot water']:
-                        for tech in json_data[state][sector][fuel][end_use].keys():
-                            stock_projections = pd.DataFrame(
-                                list(json_data[state][sector][fuel][end_use][tech]['stock'].items()),
-                                columns=['year', 'stock']
-                            )
-                            stock_projections['sector'] = sector
-                            stock_projections['fuel'] = fuel
-                            stock_projections['end_use'] = end_use
-                            stock_projections['technology'] = tech
-                            stock_projections['year'] = stock_projections['year'].astype(int)
-                            stock_projections['state'] = state
-                            stock_projections['metric'] = metric
-                            stock_projections['scenario'] = scenario
+#     building_forecast = pd.DataFrame()
+#     for state in json_data.keys():
+#         if len(json_data[state]) == 0:
+#             continue
+#         for sector in ['resid', 'comm']:
+#             for fuel in ['electricity', 'gas', 'refined liquids', 'biomass']:
+#                 try:
+#                     for end_use in ['heating', 'hot water']:
+#                         for tech in json_data[state][sector][fuel][end_use].keys():
+#                             stock_projections = pd.DataFrame(
+#                                 list(json_data[state][sector][fuel][end_use][tech]['stock'].items()),
+#                                 columns=['year', 'stock']
+#                             )
+#                             stock_projections['sector'] = sector
+#                             stock_projections['fuel'] = fuel
+#                             stock_projections['end_use'] = end_use
+#                             stock_projections['technology'] = tech
+#                             stock_projections['year'] = stock_projections['year'].astype(int)
+#                             stock_projections['state'] = state
+#                             stock_projections['metric'] = metric
+#                             stock_projections['scenario'] = scenario
 
-                            building_forecast = pd.concat([building_forecast, stock_projections])
-                except KeyError:
-                    pass
+#                             building_forecast = pd.concat([building_forecast, stock_projections])
+#                 except KeyError:
+#                     pass
 
-    return building_forecast
+#     return building_forecast
 
 
 def phase2_scout_forecast(file):
@@ -159,7 +159,7 @@ def phase2_scout_forecast(file):
     stock_by_region_class_enduse = stock_by_region_class[end_use]
     stock_by_region_class_enduse_fuel = stock_by_region_class_enduse[fuel]
 
-    first_file = '/Users/yode049/Downloads/2025-04-01 Scout Outputs/stated.json'
+    first_file = '/Users/faye994/code/BICEP/stated.json'
     first_scenario = json.load(open(first_file))
 
     for year in range(2024, 2050):
@@ -184,11 +184,14 @@ def phase2_scout_forecast(file):
             f'AL {year} stock_per_region / total stock * total cost = ${(new_stock_per_region + existing_stock_per_region) / stock * cost:,.2f}')
         print(
             f'AL {year} total cost / total stock = ${cost / stock:,.2f}')
+        
+        k=1
 
 
 if __name__ == '__main__':
-    bau_forecast = scout_forecast(bau_building_blob_name, scenario='bau')
-    high_forecast = scout_forecast(high_building_blob_name, scenario='high')
+    phase2_scout_forecast(file=None)  # Replace with actual file path if needed FIRST SCENARIO
+    # bau_forecast = scout_forecast(bau_building_blob_name, scenario='bau')
+    # high_forecast = scout_forecast(high_building_blob_name, scenario='high')
 
-    high_forecast.to_csv('scout_stock_forecast_high.csv', index=False)
-    bau_forecast.to_csv('scout_stock_forecast_bau.csv', index=False)
+    # high_forecast.to_csv('scout_stock_forecast_high.csv', index=False)
+    # bau_forecast.to_csv('scout_stock_forecast_bau.csv', index=False)
